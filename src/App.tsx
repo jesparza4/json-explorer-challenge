8e7aac7f17d16a28735a40127e95e34863561b86
Jesus Esparza <66302042+jesparza4@users.noreply.github.com>
2024-02-21 14:50:02 +0100

    chore: update readme and use explorer in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { JsonExplorer } from "./components/JsonExplorer";
 
-function App() {
+const jsonData = {
+  date: "2021-10-27T07:49:14.896Z",
+  hasError: false,
+  fields: [
+    {
+      id: "4c212130",
+      prop: "iban",
+      value: "DE81200505501265402568",
+      hasError: false,
+    },
+  ],
+};
+
+const AppContainer = styled.div`
+  margin: 24px;
+  display: flex;
+  gap: 16px;
+  flex-direction: column;
+  width: 50%;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <JsonExplorer json={jsonData} rootName="res" />
+    </AppContainer>
   );
-}
+};
 
 export default App;
